cef41e11900332ae5c091df78f77b7948e67f1b8
MRBULL93 <kejvidoko@gmail.com>
2015-11-18 02:04:51 +0100

    Test

--- ECommerceApplication/Controllers/HomeController.cs
+++ ECommerceApplication/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace ECommerceApplication.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            //Kthen View
+            //Kthen 
             return View();
         }
     }
